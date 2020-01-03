1b7f4476521b1d9981aac3c692800fa2dc9aad56
HJY <taxol1203@pusan.ac.kr>
2020-01-03 16:30:07 +0900

    이메일 뽑아냄

--- app/src/main/java/com/music961/chat/Activity/MainActivity.kt
+++ app/src/main/java/com/music961/chat/Activity/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.music961.chat.Activity
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.music961.chat.Bean.chClient
+import com.music961.chat.Bean.lo
 import com.music961.chat.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
@@ -13,7 +24,13 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
+
 class MainActivity : AppCompatActivity() {
+
+    //구글 api 변수 선언
+    private val RC_SIGN_IN = 9001
+    private var googleSigninClient: GoogleSignInClient? = null
+    private var firebaseAuth: FirebaseAuth? = null
 
     internal lateinit var preferences: SharedPreferences
 
@@ -21,6 +38,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //구글 로그인 api
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
+        googleSigninClient = GoogleSignIn.getClient(this,gso)
+        firebaseAuth = FirebaseAuth.getInstance()
+        googleLoginBtn.setOnClickListener {
+            val signinIntent = googleSigninClient?.signInIntent
+            startActivityForResult(signinIntent, RC_SIGN_IN)
+        }
+        //영진이 형
         CoroutineScope(Dispatchers.Main).launch {
             // 메인 쓰레드 UI 쓰레드 부분
             withContext(CoroutineScope(Dispatchers.IO).coroutineContext) {
@@ -42,5 +69,49 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+
+    }
+
+    //구글 로그인 api
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == RC_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try{
+                val account = task.getResult(ApiException::class.java)
+                if (account != null) {
+                    firebaseAuthWithGoogle(account)
+                }
+            }catch(e: ApiException){
+
+            }
+        }
+    }
+
+    //구글 로그인 api
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount){
+        val credential = GoogleAuthProvider.getCredential(acct.idToken,null)
+        firebaseAuth!!.signInWithCredential(credential)
+            .addOnCompleteListener(this){
+                if(it.isSuccessful){
+                    val user = firebaseAuth?.currentUser
+                    Toast.makeText(this,"로그인 성공",Toast.LENGTH_SHORT).show()
+                    firebaseAuth?.currentUser?.let {
+
+                        Log.d("로그", "이름 : ${user?.displayName}")
+                        Log.d("로그", "이메일 : ${user?.email}")
+                        Log.d("로그", "사진 : ${user?.photoUrl}")
+                        Log.d("로그", "이메일 유효성 : ${user?.isEmailVerified}")
+                        Log.d("로그", "uid : ${user?.uid}")
+
+
+                    }
+
+                }
+                else{
+                    Toast.makeText(this,"로그인 실패",Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
