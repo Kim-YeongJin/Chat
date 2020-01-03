af0d43a2dac874b898c7e2ac4b7a565060143430
HJY <taxol1203@pusan.ac.kr>
2020-01-03 14:58:38 +0900

    구글 로그인 api 사용. 로그인만

--- app/src/main/java/com/music961/chat/MainActivity.kt
+++ app/src/main/java/com/music961/chat/MainActivity.kt
@@ -1,12 +1,74 @@
 package com.music961.chat
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 class MainActivity : AppCompatActivity() {
-    //김영짖ㄴ바ㅗㅂ 
+
+    //구글 api 변수 선언
+    private val RC_SIGN_IN = 9001
+    private var googleSigninClient: GoogleSignInClient? = null
+    private var firebaseAuth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //구글 로그인 api
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
+        googleSigninClient = GoogleSignIn.getClient(this,gso)
+        firebaseAuth = FirebaseAuth.getInstance();
+        googleLoginBtn.setOnClickListener {
+            val signinIntent = googleSigninClient?.signInIntent
+            startActivityForResult(signinIntent, RC_SIGN_IN)
+        }
+
+
+
+    }
+
+    //구글 로그인 api
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode === RC_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try{
+                val account = task.getResult(ApiException::class.java)
+                if (account != null) {
+                    firebaseAuthWithGoogle(account)
+                }
+            }catch(e: ApiException){
+
+            }
+        }
+    }
+
+    //구글 로그인 api
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount){
+        val credential = GoogleAuthProvider.getCredential(acct.idToken,null)
+        firebaseAuth!!.signInWithCredential(credential)
+            .addOnCompleteListener(this){
+                if(it.isSuccessful){
+                    val user = firebaseAuth?.currentUser
+                    Toast.makeText(this,"로그인 성공",Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(this,"로그인 실패",Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
